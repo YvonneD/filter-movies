5f800efbb9a2175ea6a082a414cfeb75c3e0f5bd
Yifang Di <diyifang@ymail.com>
2022-02-08 21:59:40 -0500

    v1

---
+++ README.md
@@ -0,0 +1,4 @@
+# filter-movies
+Dev Ed tutorial---React filtering animation
+
+![](https://cdn-images-1.medium.com/max/800/1*VLRzRDV7YIN8j_035DuWew.gif)
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+import Filter from "./Filter";
+import { motion, AnimatePresence } from "framer-motion";
+
+function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=1a735fa31dc7f45dfc6d9bf3836a6a60&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div
+        layout
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        className="popular-movies">
+        <AnimatePresence>
+          {filtered.map((movie) => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+        </AnimatePresence>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
